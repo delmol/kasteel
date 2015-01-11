503f23479cd9ab41afaff15fd2d496184bdad6d8
Ben Orrin <ben.orrin@b3njee567.com>
2015-01-11 17:16:31 +0000

    Note text destroy timer
    Added timer to destroy note text after 5 seconds

--- js/Preload.js
+++ js/Preload.js
@@ -19,6 +19,7 @@ TopDownGame.Preload.prototype = {
     this.load.image('player', 'assets/images/player.png');
     this.load.image('browndoor', 'assets/images/browndoor.png');
 	this.load.image('spell1', 'assets/images/spell1.png');
+	this.load.image('blank', 'assets/images/blank.png');
     
   },
   create: function() {
